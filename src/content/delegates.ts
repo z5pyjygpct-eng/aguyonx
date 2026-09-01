a4ab1225fed235731ef018a3838f50ea473d4d19
Scott Mineo <z5pyjygpct-eng@users.noreply.github.com>
2026-09-01 18:24:38 +0000

    Add Betsy B. Carr HD-78 tax paper to the VA Delegates index.

--- src/content/delegates.ts
+++ src/content/delegates.ts
@@ -290,6 +290,13 @@ export const DELEGATE_FILES: DelegateFile[] = [
     tax: "/files/delegates/schmidt-hd77-tax.pdf",
   },
   {
+    slug: "carr-hd78",
+    name: "Betsy B. Carr",
+    district: "HD-78",
+    locality: "City of Richmond (part)",
+    tax: "/files/delegates/carr-hd78-tax.pdf",
+  },
+  {
     slug: "singh-hd26",
     name: 'Jas Jeet "JJ" Singh',
     district: "HD-26",
